f31ba141db5acb5a04673b90be9803c33984969a
a1kuat <a1kuat1511@gmail.com>
2022-09-08 20:03:39 +0500

    todo app

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-_3PFcM9iuqm6iuym1jzySbf8eupBp1s',
+  authDomain: 'todo-retake.firebaseapp.com',
+  projectId: 'todo-retake',
+  storageBucket: 'todo-retake.appspot.com',
+  messagingSenderId: '357888037320',
+  appId: '1:357888037320:web:7642e26674976d09ecb0fd'
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
